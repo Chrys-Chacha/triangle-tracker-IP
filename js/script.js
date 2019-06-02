bd4597bf1802cb806e1ed3822c857a570c98eac7
Chrys-Chacha <chryschacha@gmail.com>
2019-06-02 13:25:32 +0300

    add the third field user input collection variable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function tracker(){
     var A= parseFloat(document.getElementById("a").value);
     var B= parseFloat(document.getElementById("b").value);
+    var C= parseFloat(document.getElementById("c").value);
 };
