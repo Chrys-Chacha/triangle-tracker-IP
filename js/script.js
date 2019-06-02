1d2895ac81a79a0274f6b407b7e34b74484a7c03
Chrys-Chacha <chryschacha@gmail.com>
2019-06-02 13:54:51 +0300

    add first condition for not a triangle response

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ function tracker(){
     var A= parseFloat(document.getElementById("a").value);
     var B= parseFloat(document.getElementById("b").value);
     var C= parseFloat(document.getElementById("c").value);
+    if((A+B)<=C || (B+C)<=A || (C+A)<=B){
+        document.getElementById('response').innerHTML="Not a triangle"
+    }
 };
