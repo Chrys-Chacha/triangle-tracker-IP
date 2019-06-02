e96c0e8e1274b2ab0ed22dd6d229a18373e69d79
Chrys-Chacha <chryschacha@gmail.com>
2019-06-02 15:25:38 +0300

    add changes to the form

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function tracker(){
     var A= parseFloat(document.getElementById("a").value);
     var B= parseFloat(document.getElementById("b").value);
     var C= parseFloat(document.getElementById("c").value);
+    
     if((A+B)<=C || (B+C)<=A || (C+A)<=B){
         document.getElementById('response').innerHTML="Not a triangle"
     }
@@ -12,7 +13,7 @@ function tracker(){
         document.getElementById('response').innerHTML="Isosceles triangle"
     }
     else if ((A!==B) && (B!==C) && (C!==A)){
-        document.getElementById('response').innerHTML="scalene triangle"
+        document.getElementById('response').innerHTML="Scalene triangle"
     }
     else{
         document.getElementById('response').innerHTML="Enter the values"
